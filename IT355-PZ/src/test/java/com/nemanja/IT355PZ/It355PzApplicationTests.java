a033756402117d685453637b16fab8ffd9c97f26
Vilke00 <nemanja.vilic@gmail.com>
2022-08-17 15:21:50 +0200

    Rework projekta, implementira se DB

---
+++ IT355-PZ/src/test/java/com/nemanja/IT355PZ/It355PzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemanja.IT355PZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It355PzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
